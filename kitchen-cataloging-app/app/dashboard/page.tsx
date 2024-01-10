c85f8c4021ed5d50e0a1b68f5defc015e61a8f06
Vardan <vardan.vanyan@gmail.com>
2024-01-10 14:53:37 -0800

    created some basic login/logout functionality as well as signIn/signUp

---
+++ kitchen-cataloging-app/app/dashboard/page.tsx
@@ -0,0 +1,16 @@
+import { UserButton, currentUser } from '@clerk/nextjs';
+
+export default async function Dashboard() {
+  const user = await currentUser();
+
+  console.log(user);
+  return (
+    <div className='flex flex-col items-center justify-center h-screen bg-gray-100 p-5'>
+      <div className='absolute top-5 right-5 flex gap-2 items-center'>
+        <UserButton showName={true} afterSignOutUrl='/' />
+      </div>
+      <h1 className='text-2xl text-gray-800 mb-5'>Welcome {user?.firstName}</h1>
+      <p className='text-xl text-gray-600 mb-5'>Here are your items</p>
+    </div>
+  );
+}
--- kitchen-cataloging-app/app/page.tsx
+++ kitchen-cataloging-app/app/page.tsx
@@ -1,12 +1,28 @@
-import { UserButton, SignInButton, currentUser } from '@clerk/nextjs';
-
+import { UserButton, currentUser } from '@clerk/nextjs';
+import Link from 'next/link';
 export default async function Home() {
   const user = await currentUser();
 
   return (
     <div className='flex flex-col items-center justify-center h-screen bg-gray-100 p-5'>
-      <h1 className='text-2xl text-gray-800 mb-5'>Home Page</h1>
-      {user ? <UserButton /> : <SignInButton />}
+      <h1 className='text-2xl text-gray-800 mb-5'>Kitchen Cataloging App</h1>
+      <p className='text-xl text-gray-600 mb-5'>
+        Please sign in to manage your kitchen items
+      </p>
+      <div className='flex gap-4'>
+        <Link
+          href='/sign-in'
+          className='px-4 py-2 bg-blue-500 text-white rounded'
+        >
+          Sign In
+        </Link>
+        <Link
+          href='/sign-up'
+          className='px-4 py-2 bg-green-500 text-white rounded'
+        >
+          Sign Up
+        </Link>
+      </div>
     </div>
   );
 }
