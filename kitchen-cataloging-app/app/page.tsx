3e70f7e95d57324b87b227b310a94b0852c68dde
Vardan <vardan.vanyan@gmail.com>
2024-01-10 11:51:43 -0800

    added auth with clerk, created signIn and signUp routes

--- kitchen-cataloging-app/app/page.tsx
+++ kitchen-cataloging-app/app/page.tsx
@@ -4,8 +4,8 @@ export default async function Home() {
   const user = await currentUser();
 
   return (
-    <div className='h-screen'>
-      <h1>Home Page</h1>
+    <div className='flex flex-col items-center justify-center h-screen bg-gray-100 p-5'>
+      <h1 className='text-2xl text-gray-800 mb-5'>Home Page</h1>
       {user ? <UserButton /> : <SignInButton />}
     </div>
   );
