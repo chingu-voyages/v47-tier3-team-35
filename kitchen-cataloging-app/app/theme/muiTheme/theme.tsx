d6f2ec42cd4d003a1211a09dbfd497c7ccc06fa7
Arky Asmal <arkyasmal@gmail.com>
2024-02-01 16:48:02 -0500

    restored lost mui typography styles

--- kitchen-cataloging-app/app/theme/muiTheme/theme.tsx
+++ kitchen-cataloging-app/app/theme/muiTheme/theme.tsx
@@ -3,6 +3,16 @@ import { ThemeProvider, createTheme } from "@mui/material";
 import palatte from "./palatte";
 import typography from "./typography";
 declare module "@mui/material/styles" {
+  interface TypographyVariants {
+    body3: React.CSSProperties;
+    body4: React.CSSProperties;
+  }
+
+  // allow configuration using `createTheme`
+  interface TypographyVariantsOptions {
+    body3?: React.CSSProperties;
+    body4?: React.CSSProperties;
+  }
   interface BreakpointOverrides {
     xxs: true;
     xs: true;
@@ -13,9 +23,35 @@ declare module "@mui/material/styles" {
   }
 }
 
+// Update the Typography's variant prop options
+declare module "@mui/material/Typography" {
+  interface TypographyPropsVariantOverrides {
+    body3: true;
+    body4: true;
+  }
+}
 const theme = createTheme({
   palette: palatte,
-  typography: typography,
+  typography: {
+    ...typography,
+    body3: {
+      fontSize: "0.875rem",
+    },
+    body4: {
+      fontSize: "0.625rem",
+    },
+  },
+  components: {
+    MuiTypography: {
+      defaultProps: {
+        variantMapping: {
+          // Map the new variant to render a <span> by default
+          body3: "span",
+          body4: "span",
+        },
+      },
+    },
+  },
   breakpoints: {
     values: {
       xxs: 0,
